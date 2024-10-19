6b3f11ffb7813747656ad7a38aa4960a10fa790b
trolol554 <149227319+trolol554@users.noreply.github.com>
2024-10-19 13:48:37 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1729319233,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"images/redcrystal-sheet0.png",
+		"images/playerred_walk-sheet0.png",
+		"images/sprite20-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
